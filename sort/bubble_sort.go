ed098490ab2c02a4911087170e48f1b0094ee1de
Mauricio Matter Donato <mmatterdonato@gmail.com>
2023-11-04 18:40:45 -0300

    feat: add implementation for Bubble Sort

---
+++ sort/bubble_sort.go
@@ -0,0 +1,15 @@
+package sort
+
+func BubbleSort(arr []int) []int {
+	len := len(arr)
+
+	for i := 0; i < len; i++ {
+		for j := 0; j < len-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+
+	return arr
+}
